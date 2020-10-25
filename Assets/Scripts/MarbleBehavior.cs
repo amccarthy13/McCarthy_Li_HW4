37546c7f694b6dc6b1ac1106173564afc4bb3a0c
Andrew McCarthy <amccarthy13@cs.uchicago.edu>
2020-10-24 20:02:15 -0500

    feat(complete): complete HW

---
+++ Assets/Scripts/MarbleBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleBehavior : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 5;
+    
+    private float fbInput;
+    private float lrInput;
+    
+    private Rigidbody _rb;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fbInput = Input.GetAxis("Vertical") * moveSpeed;
+        lrInput = Input.GetAxis("Horizontal") * rotateSpeed;
+    
+        this.transform.Translate(Vector3.forward * fbInput * 
+        Time.deltaTime);
+        this.transform.Rotate(Vector3.up * lrInput * Time.deltaTime);
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * lrInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation *
+            Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position +
+            this.transform.forward * fbInput * Time.fixedDeltaTime);
+
+         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+    
+}
