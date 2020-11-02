0a9155ed2230b55400aceb40a9a42a16308a4c5b
Andrew McCarthy <amccarthy13@cs.uchicago.edu>
2020-11-02 13:46:56 -0600

    finish

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameBehavior : MonoBehaviour
 {
     public static GameBehavior instance;
+    public Text Status;
+    public Text endText;
 	public float marbleHealth;
 	public float goalsCollected;
+    GameObject[] endObjects;
+
+    public GameObject canvas;
     // Start is called before the first frame update
+
     void Start()
     {
+        Time.timeScale = 1;
+        endObjects = GameObject.FindGameObjectsWithTag("ShowOnEnd");
+        hideEnd();
+        Status = GameObject.Find("Status").GetComponent<Text>();
         instance = this;
         instance.marbleHealth = 100;
         instance.goalsCollected = 0;
@@ -18,6 +29,52 @@ public class GameBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool endTrigger = false;
+
+
+
+        if (marbleHealth <= 0) 
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showEnd();
+                endText = GameObject.Find("EndText").GetComponent<Text>();
+                endText.text = "game over, you lose!";
+            }
+        }
+
+        else if (goalsCollected >= 4) 
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showEnd();
+                endText = GameObject.Find("EndText").GetComponent<Text>();
+                endText.text = "You Win!";
+            }
+        }
+        Status.text = marbleHealth.ToString() + "          " + goalsCollected.ToString();
+
+    }
+
+    public void Reload() 
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void showEnd()
+    {
+        foreach(GameObject g in endObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+    public void hideEnd()
+    {
+        foreach(GameObject g in endObjects)
+        {
+            g.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehavior : MonoBehaviour
+{
+	  public float obstacleHealth = 100;
+    void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.name == "Blast(Clone)")
+       {
+       		obstacleHealth -= 10;
+       		if (obstacleHealth <= 0) 
+       		{
+       			Destroy(this.gameObject);
+       		}
+       }
+    }
+}
